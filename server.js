ad6a1ba6e670df890c8d2663157dd119c8edbb8a
Torstb18 <manchester2009@live.no>
2021-02-08 17:09:29 +0100

    gjorde om på server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.listen(PORT, ()=>{
     console.log('Running on port: '+ PORT);
