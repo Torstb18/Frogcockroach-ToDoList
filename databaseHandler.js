29b6ee13e335b5f67b0ff8feb338d5c0c697fecd
Torstb18 <manchester2009@live.no>
2021-02-08 16:36:36 +0100

    gjort om i databasehandler

--- databaseHandler.js
+++ databaseHandler.js
@@ -1,2 +1,9 @@
 const pg = require('pg');
 const dbCredentials = process.env.DATABASE_URL || require('./localenv').credentials;
+
+const pool = new pg.Pool({
+    connectionString: dbCredentials,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
