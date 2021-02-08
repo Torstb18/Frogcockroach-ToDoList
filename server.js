f12b58155e30c5f24f0fccb43e53a4a5644049c6
Torstb18 <manchester2009@live.no>
2021-02-08 14:53:16 +0100

    oppdatert gitignore, databasehandler og server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
-const databaseHandler = require ('./databaseHandler.js')
+const databaseHandler = require ('./databaseHandler.js');
 const app = express();
-const port = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.listen(port, ()=>{
-    console.log('Running on port: '+ port);
+app.listen(PORT, ()=>{
+    console.log('Running on port: '+ PORT);
 });
