665c96ff59f38c69723b8d04c1aa5e16810a0c76
Torstb18 <manchester2009@live.no>
2021-02-10 16:37:49 +0100

    små fiksinger

--- databaseHandler.js
+++ databaseHandler.js
@@ -1,50 +1,56 @@
 const pg = require("pg");
-const dbCredentials = process.env.DATABASE_URL || require("./localenv").credentials;
+const dbCredentials =
+  process.env.DATABASE_URL || require("./localenv").credentials;
 
 class StorageHandler {
+  constructor(credentials) {
+    this.credentials = {
+      connectionString: credentials,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    };
+  }
 
-    constructor(credentials) {
-        this.credentials = {
-            connectionString: credentials,
-            ssl: {
-                rejectUnauthorized: false
-            }
-        };
+  async insertUser(username, password) {
+    const client = new pg.Client(this.credentials);
+    let results = null;
+    try {
+      await client.connect();
+      results = await client.query(
+        'INSERT INTO "public"."user"("username", "password") VALUES($1, $2) RETURNING *;',
+        [username, password]
+      );
+      results = results.rows[0].message;
+      client.end();
+    } catch (err) {
+      client.end();
+      console.log(err);
+      results = err;
     }
 
-    async insertUser(username, password) {
-        const client = new pg.Client(this.credentials);
-        let results = null;
-        try {
-            await client.connect();
-            results = await client.query('INSERT INTO "public"."user"("username", "password") VALUES($1, $2) RETURNING *;', [username, password]);
-            results = results.rows[0].message;
-            client.end();
-        } catch (err) {
-            client.end();
-            console.log(err);
-            results = err;
-        }
+    return results;
+  }
 
-        return results;
+  async checkUser(username) {
+    const client = new pg.Client(this.credentials);
+    let results = null;
+    try {
+      await client.connect();
+      results = await client.query(
+        "SELECT password FROM public.user WHERE username = $1;",
+        [username]
+      );
+      results = results.rows[0].password;
+      client.end();
+    } catch (err) {
+      client.end();
+      console.log(err);
+      results = err;
     }
 
-    async checkUser(username) {
-        const client = new pg.Client(this.credentials);
-        let results = null;
-        try {
-            await client.connect();
-            results = await client.query('SELECT password FROM public.user WHERE username = $1;', [username]);
-           results = results.rows[0].password;
-            client.end();
-        } catch (err) {
-            client.end();
-            console.log(err);
-            results = err;
-        }
-
-        return results;
-    }
+    return results;
+  }
 }
 
 module.exports = new StorageHandler(dbCredentials);
--- server.js
+++ server.js
@@ -1,60 +1,72 @@
-const express = require ('express');
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
+const express = require("express");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
 const secret = process.env.hashSecret || require("./localenv").hashSecret;
-const databaseHandler = require ('./databaseHandler.js');
+const databaseHandler = require("./databaseHandler.js");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 let login = false;
 const auth = async (req, res, next) => {
-    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
-        return res.append("WWW-Authenticate", 'Basic realm="User Visible Realm", charset="UTF-8"').status(401).end()
-    }
-    const credentials = req.headers.authorization.split(' ')[1];
-    const [username, password] = Buffer.from(credentials, 'base64').toString('UTF-8').split(":");
+  if (
+    !req.headers.authorization ||
+    req.headers.authorization.indexOf("Basic ") === -1
+  ) {
+    return res
+      .append(
+        "WWW-Authenticate",
+        'Basic realm="User Visible Realm", charset="UTF-8"'
+      )
+      .status(401)
+      .end();
+  }
+  const credentials = req.headers.authorization.split(" ")[1];
+  const [username, password] = Buffer.from(credentials, "base64")
+    .toString("UTF-8")
+    .split(":");
 
-    let data = await databaseHandler.checkUser(username);
-    console.log(data);
+  let data = await databaseHandler.checkUser(username);
+  console.log(data);
 
-    const cryptPassword = crypto.createHmac('sha256', secret)
+  const cryptPassword = crypto
+    .createHmac("sha256", secret)
     .update(password)
-    .digest('hex');
+    .digest("hex");
 
-    if(data===cryptPassword){
-        login=true;
-    }else{
-        login=false;
-    }
-    next();
-}; 
+  if (data === cryptPassword) {
+    login = true;
+  } else {
+    login = false;
+  }
+  next();
+};
 
-app.post("/createUser", async(req, res)=>{
-    let username = req.body.user;
-    let password = req.body.password;
+app.post("/createUser", async (req, res) => {
+  let username = req.body.user;
+  let password = req.body.password;
 
-    const cryptPassword = crypto.createHmac('sha256', secret)
-                   .update(password)
-                   .digest('hex');
+  const cryptPassword = crypto
+    .createHmac("sha256", secret)
+    .update(password)
+    .digest("hex");
 
-    console.log(username);
-    console.log(cryptPassword);
+  console.log(username);
+  console.log(cryptPassword);
 
-    databaseHandler.insertUser(username, cryptPassword);
-})
+  databaseHandler.insertUser(username, cryptPassword);
+});
 
-app.get("/login", auth, async(req, res)=>{
-    if(login){
-        res.status(200).end();
-    }else{
-        res.status(401).end();
-    }
-    
-})
+app.get("/login", auth, async (req, res) => {
+  if (login) {
+    res.status(200).end();
+  } else {
+    res.status(401).end();
+  }
+});
 
-app.listen(PORT, ()=>{
-    console.log('Running on port: '+ PORT);
+app.listen(PORT, () => {
+  console.log("Running on port: " + PORT);
 });
